08f6fd528972e5d1b0643be4867ad8b9e8562459
Stephen <projects@ithots.com>
2025-09-04 13:51:56 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,46 @@
+import os
+import io
+from flask import Flask, request, render_template, send_file, url_for
+from rembg import remove
+from PIL import Image
+
+UPLOAD_FOLDER = 'uploads'
+RESULT_FOLDER = 'results'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(RESULT_FOLDER, exist_ok=True)
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['RESULT_FOLDER'] = RESULT_FOLDER
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['image']
+        if file:
+            input_path = os.path.join(UPLOAD_FOLDER, file.filename)
+            file.save(input_path)
+            with open(input_path, 'rb') as inp:
+                output = remove(inp.read())
+            output_img = Image.open(io.BytesIO(output))
+            output_path = os.path.join(RESULT_FOLDER, file.filename + '.png')
+            output_img.save(output_path)
+            from flask import url_for
+            result_filename = file.filename + '.png'
+            preview_img = url_for('result_file', filename=result_filename)
+            download_link = url_for('result_file', filename=result_filename)
+            return render_template('index.html',
+                                   preview_img=preview_img,
+                                   download_link=download_link)
+    return render_template('index.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_file(os.path.join(UPLOAD_FOLDER, filename))
+
+@app.route('/results/<filename>')
+def result_file(filename):
+    return send_file(os.path.join(RESULT_FOLDER, filename))
+
+if __name__ == '__main__':
+    app.run(debug=True)
